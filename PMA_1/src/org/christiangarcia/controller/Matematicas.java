3d02d55e8c222b6b6b53705d983a5f1c1ea4c5a5
divCancerTactico777 <cgarcia-2023529@kinal.edu.gt>
2026-06-10 22:46:50 -0600

    001 - Agregamos la calculadora de salarios

--- PMA_1/src/org/christiangarcia/controller/Matematicas.java
+++ PMA_1/src/org/christiangarcia/controller/Matematicas.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.christiangarcia.controller;
 
-/**
- *
- * @author HP
- */
 public class Matematicas {
-    
+
+    public double calcularSalario(double tiempo, double pagoHora) {
+        return tiempo * pagoHora;
 }
