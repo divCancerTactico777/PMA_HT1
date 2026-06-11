33488154f306c8ff37fc919db89ae9324b6437c9
divCancerTactico777 <cgarcia-2023529@kinal.edu.gt>
2026-06-10 23:15:42 -0600

    005 - Agregamos la calculadora del perímetro de un círculo

--- PMA_1/src/org/christiangarcia/controller/Matematicas.java
+++ PMA_1/src/org/christiangarcia/controller/Matematicas.java
@@ -15,6 +15,11 @@ public double promedio(double n1, double n2, double n3) {
     return (n1 + n2 + n3)/3.0;
 }
 
-public  double calculadoraGradosCaF(double celsius)
+public  double calculadoraGradosCaF(double celsius) {
     return (celsius * (9.0/5.0) + 32.0;
 }
+
+public double perimetroCirculo(double radio) {
+    return 2.0 * Math.PI * radio;
+}
+    
