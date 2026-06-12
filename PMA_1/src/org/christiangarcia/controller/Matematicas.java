9753a0781cac2958d2176218875fa770e055eba9
divCancerTactico777 <cgarcia-2023529@kinal.edu.gt>
2026-06-11 18:33:00 -0600

    010_2

--- PMA_1/src/org/christiangarcia/controller/Matematicas.java
+++ PMA_1/src/org/christiangarcia/controller/Matematicas.java
@@ -2,43 +2,43 @@
 package org.christiangarcia.controller;
 
 public class Matematicas {
-
+//001
     public double calcularSalario(double tiempo, double pagoHora) {
         return tiempo * pagoHora;
 }
-    
+//002   
 public double areaTriangulo(double base, double altura) {
     return (base * altura)/2.0;
 }
-
+//003
 public double promedio(double n1, double n2, double n3) {
     return (n1 + n2 + n3)/3.0;
 }
-
+//004
 public  double calculadoraGradosCaF(double celsius) {
-    return (celsius * (9.0/5.0) + 32.0;
+    return celsius * (9.0/5.0) + 32.0;
 }
-
+//005
 public double perimetroCirculo(double radio) {
     return 2.0 * Math.PI * radio;
 }
-
+//006
 public double volumenCilindro(double radio, double h) {
-    return Math.PI * Math.pow(radio, 2) * altura;
+    return Math.PI * Math.pow(radio, 2) * h;
 }
-
+//007
 public double hipotenusa(double c1, double c2) {
-    return Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2);
+    return Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
     {
-
+//008
 public double diasMinutos(double dias) {
     return dias * 144.0;
     }
-    
+//009
 public double restoDivision(double dividendo, souble divisor) {
     return dividendo % divisor;
 }
-    
+//010    
 public int potenciaCubo(int lado) {
     return lado * lado * lado;
 }
