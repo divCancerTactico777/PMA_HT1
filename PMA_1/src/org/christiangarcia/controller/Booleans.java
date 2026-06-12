25bfd659b31c7c1a5706b2ec06c2518d2ddf6aac
divCancerTactico777 <cgarcia-2023529@kinal.edu.gt>
2026-06-11 18:45:39 -0600

    011 Agregamos el verificador de mayor de edad

--- PMA_1/src/org/christiangarcia/controller/Booleans.java
+++ PMA_1/src/org/christiangarcia/controller/Booleans.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.christiangarcia.controller;
 
-/**
- *
- * @author HP
- */
 public class Booleans {
+
+public boolean mayorEdad(int edad) {
+    return edad >= 18;
+}
     
 }
