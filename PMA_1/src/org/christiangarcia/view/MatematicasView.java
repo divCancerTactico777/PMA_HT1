39f979e9b07e94a86c103565032c67cc7ab3672d
divCancerTactico777 <cgarcia-2023529@kinal.edu.gt>
2026-06-12 21:24:17 -0600

    Agregamos la estructura de MatematicasView

--- PMA_1/src/org/christiangarcia/view/MatematicasView.java
+++ PMA_1/src/org/christiangarcia/view/MatematicasView.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package org.christiangarcia.view;
 
-/**
- *
- * @author HP
- */
+package org.christiangarcia.view;
+import java.util.Scanner;
+
 public class MatematicasView {
-    
+
+    private final Scanner scanner;
+
+    public MatematicasView() {
+        this.scanner = new Scanner(System.in);
+    }
+    public int pedirInt(String mensaje) {
+        System.out.println(mensaje);
+        int valor = scanner.nextInt();
+        scanner.nextLine();
+        return valor;
+    }
+    public int menuMatematicas() {
+        System.out.println("--- Operaciones Matemáticas ---");
+        System.out.println("1. Calculadora de Salario");
+        System.out.println("2. Area de un Triangulo");
+        System.out.println("3. Promedio Academico");
+        System.out.println("4. Convertir de Temperatura");
+        System.out.println("5. Perimetro de Circulo");
+        System.out.println("6 Volumen de cilindro");
+        System.out.println("7. Hipotenusa");
+        System.out.println("8. Dias a Minutos");
+        System.out.println("9. Division");
+        System.out.println("10. Potencia al cubo");
+        System.out.println("0. Regresar al Menu Principal");
+        System.out.println("-----------------------------");
+        System.out.print("Elija una opción:");
+        
+        int opcion = scanner.nextInt();
+        scanner.nextLine(); 
+        return opcion;
+    }
+    public double pedirDouble(String mensaje) {
+        System.out.print(mensaje);
+        double valor = scanner.nextDouble();
+        scanner.nextLine();
+        return valor;
+    }
+    public void mostrarResultado(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
 }
