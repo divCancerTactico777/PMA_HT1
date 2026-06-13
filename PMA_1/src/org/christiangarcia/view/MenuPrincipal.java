8659e9c35420063d4ac862b85e60ee8f70901f59
divCancerTactico777 <cgarcia-2023529@kinal.edu.gt>
2026-06-12 21:14:02 -0600

    Realizamos estructura de Menu Principal

--- PMA_1/src/org/christiangarcia/view/MenuPrincipal.java
+++ PMA_1/src/org/christiangarcia/view/MenuPrincipal.java
@@ -1,14 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.christiangarcia.view;
 
 import java.util.Scanner;
-import org.christiangarcia.controller.Booleans;
-import org.christiangarcia.controller.Strings;
-import org.christiangarcia.controller.Matematicas;
 
-public class MenuPrincipal {
+public class MenuPrincipalView {
+    private final Scanner scanner;
 
+    public MenuPrincipalView() {
+        this.scanner = new Scanner(System.in);
+    }
+    public int mostrarMenuPrincipal()
+        System.out.println("            Menú Principal            ");
+        System.out.println("--------------------------------------");
+        System.out.println("1. Matematicas");  
+        System.out.println("2. Cadenas");  
+        System.out.println("3. Booleans");       
+        System.out.println("4. Salir");                            
+        System.out.println("--------------------------------------");
+        System.out.println("Seleccione una opcion: ");
+        
+        int opcion = scanner.nextInt();
+        scanner.nextLine(); 
+        return opcion;
+    }
+
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
 }
